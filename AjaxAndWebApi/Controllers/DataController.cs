65d46c60ca32427b7f8452353b71c3370a921859
Henrik Obsen <hto@e-train.dk>
2017-10-10 13:46:27 +0200

    Upload

--- AjaxAndWebApi/Controllers/DataController.cs
+++ AjaxAndWebApi/Controllers/DataController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.IO;
 
 namespace AjaxAndWebApi.Controllers
 {
@@ -21,7 +22,7 @@ namespace AjaxAndWebApi.Controllers
 
         [Route("api/Data/Plus/{tal1?}/{tal2?}")]
         [HttpGet]
-        public int Plus(int tal1=0, int tal2=0)
+        public int Plus(int tal1 = 0, int tal2 = 0)
         {
             return tal1 + tal2;
         }
@@ -54,7 +55,7 @@ namespace AjaxAndWebApi.Controllers
         [HttpGet]
         public void DeletePerson(int id)
         {
-            pf.Delete(id);           
+            pf.Delete(id);
         }
 
         [Route("api/Data/UpdatePerson/")]
@@ -65,13 +66,58 @@ namespace AjaxAndWebApi.Controllers
             if (ModelState.IsValid)
             {
                 pf.Update(p);
-            }    
+            }
             else
             {
                 valid = false;
             }
 
             return valid;
+        }
+
+        [Route("api/Data/UploadFile/")]
+        [HttpPost()]
+        public string UploadFiles()
+        {
+            Uploader u = new Uploader();
+
+            int iUploadedCnt = 0;
+                        
+            string sPath = "";
+            sPath = System.Web.Hosting.HostingEnvironment.MapPath("~/Images/");
+
+            System.Web.HttpFileCollection hfc = System.Web.HttpContext.Current.Request.Files;
+
+            // CHECK THE FILE COUNT.
+            for (int iCnt = 0; iCnt <= hfc.Count - 1; iCnt++)
+            {
+                System.Web.HttpPostedFile hpf = hfc[iCnt];
+
+                if (hpf.ContentLength > 0)
+                {
+                    // CHECK IF THE SELECTED FILE(S) ALREADY EXISTS IN FOLDER. (AVOID DUPLICATE)
+                    if (!File.Exists(sPath + Path.GetFileName(hpf.FileName)))
+                    {
+                        string filen = Path.GetFileName(hpf.FileName);
+                        // SAVE THE FILES IN THE FOLDER.
+                        hpf.SaveAs(sPath + filen);
+                        u.ReSizeImage(sPath + filen, sPath + "Thump/", 120);
+
+                        iUploadedCnt = iUploadedCnt + 1;
+                    }
+                }
+            }
+
+            // RETURN A MESSAGE.
+            if (iUploadedCnt > 0)
+            {
+                return iUploadedCnt + " billeder uploadet!";
+            }
+            else
+            {
+                return "Upload Failed!!!";
+            }
+
         }
     }
 }
